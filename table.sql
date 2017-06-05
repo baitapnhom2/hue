aaba11c32eae607704916ffbf47294926fdd5707
DuongHue89 <duonghue31@gmail.com>
2017-06-05 16:59:41 +0700

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,26 @@
+﻿create table Nhansu (
+ten nvarchar(50) not null ,
+ma varchar(10) primary key,
+ngaysinh datetime not null,
+quequan nvarchar(100) not null,
+hesoluong varchar(20) not null,
+phongbanma varchar(10)  not null ,
+--ma varchar(10) references bomon(ma) notnull,
+);
+create table phongban(
+ma varchar(10) primary key not null,
+ten nvarchar(50) not null ,
+diachi nvarchar(50) not null ,
+sodienthoai nvarchar(20) not null,
+);
+create table hopdong(
+ma varchar(10) not null,
+ten nvarchar(50) not null,
+ngayki datetime not null ,
+ngayhethan datetime not null,
+nhanvienma varchar(10),
+);
+create table dangnhap(
+user nvarchar(20) notnull,
+pass varchar(10) not null,
+);
